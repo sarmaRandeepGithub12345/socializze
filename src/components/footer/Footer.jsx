d06468899ddcddb62c495a3f3e915c3b6ee5ee3c
Randeep <randeepsarma10@gmail.com>
2025-06-03 19:18:22 +0530

    Done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router";
+
 export const Footer = () => {
   return (
     <footer className="bg-gray-100 text-center py-4 shadow-inner">
       <p className="text-sm text-gray-600">
         © 2025 Socializze. All rights reserved. •{" "}
-        <a
+        <Link
           href="/privacy-policy"
           className="text-blue-500 hover:underline cursor-pointer"
         >
           Privacy Policy
-        </a>
+        </Link>
       </p>
     </footer>
   );
